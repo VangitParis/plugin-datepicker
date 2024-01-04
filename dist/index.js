a65aaff02d4593ad2c9bee17a36ce3a9f3fe9a7a
VangitParis <102918421+VangitParis@users.noreply.github.com>
2024-01-04 11:03:23 +0100

    Ajouter les fichiers d'index pour la publication sur NPM

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,5 @@
-"use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-Object.defineProperty(exports, "DatePicker", {
-  enumerable: true,
-  get: function () {
-    return _DatePicker.default;
-  }
-});
-var _DatePicker = _interopRequireDefault(require("./DatePicker"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+import DatePicker from '../src/components/DatePicker';
+
+// Export principal component
+export default DatePicker;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+// Import files for DatePicker component
+import DatePicker from './DatePicker';
+import Calendar from './Calendar';
+
+
+// Export all components
+export { DatePicker, Calendar };
